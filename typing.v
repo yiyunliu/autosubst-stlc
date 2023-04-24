6b34f66d62dece2dbf59b5baaa3dd382fa4ebbf2
Yiyun Liu <liuyiyun@seas.upenn.edu>
2023-04-23 21:23:07 -0400

    Add SAT (WIP)

--- typing.v
+++ typing.v
@@ -179,3 +179,187 @@ Proof.
   - hauto inv:- ctrs:Red l:on use:empty_lam_pi_inv.
   - sfirstorder.
 Qed.
+
+Inductive step {n : nat} : tm n -> tm n -> Prop :=
+| S_AppAbs (a : tm (S n)) (b : tm n) :
+  step (app (lam a) b) (subst_tm (scons b ids) a)
+| S_App0 (a a' b : tm n) :
+  step a a' ->
+  step (app a b) (app a' b)
+| S_App1 (a b b' : tm n) :
+  step b b' ->
+  step (app a b) (app a b').
+
+Definition rstep {n : nat} (a b : tm n) := step b a.
+
+Definition sn {n : nat} := Acc (@rstep n).
+
+(* SNe is equivalent to the definition of Base *)
+Inductive SNe {n : nat} : tm n -> Prop :=
+| SNe_Var x :
+  (* ------------- *)
+  SNe (var_tm x)
+
+| SNe_Unit :
+(* ------------ *)
+  SNe unit
+
+| SNe_App a b :
+  SNe a ->
+  SN b ->
+  (* -------------- *)
+  SNe (app a b)
+
+with SN {n : nat} : tm n -> Prop :=
+| SN_Lam a0 :
+  SN a0 ->
+(* ------------------ *)
+  SN (lam a0)
+
+| SN_BaseIncl a :
+  SNe a ->
+(* -------- *)
+  SN a
+
+| SN_BackExp a b :
+  SN_Red a b ->
+  SN b ->
+(* ------- *)
+  SN a
+
+with SN_Red {n : nat} : tm n -> tm n -> Prop :=
+| SNR_AppAbs a b:
+  SN b ->
+  (* --- *)
+  SN_Red (app (lam a) b) (subst_tm (b..) a)
+
+| SNR_App a a' b :
+  SN_Red a a' ->
+  (* --------- *)
+  SN_Red (app a b) (app a' b).
+
+Scheme SNe_ind' := Induction for SNe Sort Prop
+  with SN_ind' := Induction for SN Sort Prop
+  with SN_Red_ind' := Induction for SN_Red Sort Prop.
+
+Combined Scheme sn_mutual from SNe_ind', SN_ind', SN_Red_ind'.
+
+#[export]Hint Constructors SNe SN SN_Red : core.
+
+Check sn_mutual.
+
+Inductive sn_Red {n : nat} : tm n -> tm n -> Prop :=
+| snR_AppAbs a b:
+  sn b ->
+  (* --- *)
+  sn_Red (app (lam a) b) (subst_tm (b..) a)
+
+| snR_App a a' b :
+  sn_Red a a' ->
+  (* --------- *)
+  sn_Red (app a b) (app a' b).
+
+Lemma SN_sound_mutual :
+  forall n,
+  (forall (a : tm n), SNe a -> sn a) /\
+  (forall (a : tm n), SN a -> sn a) /\
+  (forall (a b : tm n), SN_Red a b -> sn_Red a b).
+Proof.
+  apply sn_mutual; eauto.
+  - hauto q:on inv:step ctrs:Acc.
+  - hauto q:on inv:step ctrs:Acc.
+  - admit.
+  - hauto q:on inv:step ctrs:Acc.
+  - admit.
+  - hauto l:on inv:step ctrs:Acc.
+  - hauto l:on inv:step ctrs:Acc.
+Admitted.
+
+
+(* Semantic arrow *)
+Definition SArr {n : nat} (A B : tm n -> Prop) (F : tm n) :=
+  forall a, A a -> B (app F a).
+
+Fixpoint Denote {n : nat} (T : ty) : tm n -> Prop :=
+  match T with
+  | TUnit => SN
+  | TPi A B => SArr (Denote A) (Denote B)
+  end.
+
+Fixpoint Base {n : nat} (a : tm n) :=
+  match a with
+  | var_tm x => True
+  | app a1 a2 => Base a1 /\ sn a2
+  | lam a => False
+  | unit => True
+  end.
+
+(* Lemma Base_0_impossible : *)
+(*   forall (a : tm 0), ~ Base a. *)
+(* Proof. *)
+(*   dependent induction a; sfirstorder. *)
+(* Qed. *)
+
+Definition SAT {n : nat} (A : tm n -> Prop) :=
+
+    (forall a, A a -> sn a) /\  (* subset condition *)
+    (forall a, Base a -> A a) /\ (* 4.3.2.1(a) *)
+      (forall a b, sn_Red a b -> A b -> A a) (* 4.3.2.1(b) *).
+
+(* Prove that Base a -> a will never reduce to a lambda? *)
+(* Lemma Base_is_SN {n : nat} (a : tm n) : *)
+(*   Base a -> *)
+(*   (* ----- *) *)
+(*   SN a. *)
+(* Proof. *)
+(*   elim:n/a. *)
+(*   - sfirstorder. *)
+(*   - sfirstorder. *)
+(*   - (* t will never be a lambda *) *)
+(*   - sfirstorder. *)
+
+
+#[export]Hint Unfold SAT : core.
+(* Lemma 4.3.3.1 *)
+Lemma SN_is_SAT {n : nat} : @SAT n sn.
+Proof.
+  rewrite /SAT; eauto.
+  split; auto.
+  split.
+  move => a.
+  elim : n / a.
+  - hauto q:on inv:step ctrs:Acc.
+  - sfirstorder.
+  - cbn.
+    move => n a ih0 b ih1 [ha hb].
+    (* Base a /\ sn b should imply that (app a b) is normalizing *)
+    have : Base (app a b) by sfirstorder.
+
+Qed.
+
+Lemma SArr_SAT {n : nat} (A B : tm n -> Prop) :
+  SAT A -> SAT B -> SAT (SArr A B).
+Proof.
+  rewrite/ SAT.
+  move => [h00 [h01 h02]] [h10 [h11 h12]].
+  split.
+  - rewrite /SArr.
+    move => a.
+    move /(_ unit ltac:(sfirstorder)).
+    move /h10.
+    move => h0.
+    admit.
+  - split.
+    + dependent induction a; sfirstorder.
+    + sfirstorder.
+Admitted.
+
+Lemma Denote_SAT {n : nat} (T : ty) : @SAT n (Denote T).
+Proof.
+  move : n.
+  elim : T.
+  - move => /= n.
+    apply : SN_is_SAT.
+  - move => /= T h0 T1 h1 n.
+    hauto l:on use:SArr_SAT.
+Qed.
